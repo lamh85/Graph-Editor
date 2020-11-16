35ee68d29ad967dca4568e849dea6f79c9a7db29
Hon Lam <hon@junglescout.com>
2020-11-15 21:38:45 -0800

    Dynamically create SVG elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,48 @@ const LINE_ATTRIBUTES = {
   strokeWidth: 2
 }
 
-const CIRCLE_ATTRIBUTES = {
-  fill: 'blue',
-  strokeWidth: 3,
-  stroke: 'black'
+const vertices = [
+  { id: 1, x: 40, y: 40 },
+  { id: 2, x: 200, y: 40 },
+  { id: 3, x: 200, y: 200 }
+]
+
+const edges = [
+  [vertices[0], vertices[1]],
+  [vertices[1], vertices[2]],
+  [vertices[2], vertices[0]],
+]
+
+const renderLine = (endPoints, index) => {
+  const attributes = {
+    x1: endPoints[0].x,
+    y1: endPoints[0].y,
+    x2: endPoints[1].x,
+    y2: endPoints[1].y,
+    stroke: 'red',
+    strokeWidth: 2,
+    key: index
+  }
+
+  return <line {...attributes} />
+}
+
+const Circle = ({ x, y }) => {
+  return <circle cx={x} cy={y} fill="blue" strokeWidth="3" stroke="black" r="20" />
 }
 
 const App = props => {
+  console.log(edges)
 
   return (
     <svg height="500" width="500">
-      <line x1="40" y1="40" x2="200" y2="40" {...LINE_ATTRIBUTES} />
-      <line x1="200" y1="40" x2="200" y2="200" {...LINE_ATTRIBUTES} />
-      <line x1="40" y1="40" x2="200" y2="200" {...LINE_ATTRIBUTES} />
+      {
+        edges.map((edge, index) => renderLine(edge, index))
+      }
 
-      <circle cx="40" cy="40" r="20" {...CIRCLE_ATTRIBUTES} />
-      <circle cx="200" cy="40" r="20" {...CIRCLE_ATTRIBUTES} />
-      <circle cx="200" cy="200" r="20" {...CIRCLE_ATTRIBUTES} />
+      {
+        vertices.map((vertex, index) => <Circle x={vertex.x} y={vertex.y} key={index} />)
+      }
     </svg>
   )
 }
