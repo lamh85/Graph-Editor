db1d5bd9691ad1b305f6dbd541ff6bd5dec9cf3a
Hon Lam <hon@junglescout.com>
2020-11-15 20:16:36 -0800

    Install babel. Config webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
