fcc0f89f13405538b14e9b1b26164f11b325dd8b
Hon Lam <hon@junglescout.com>
2020-12-29 17:09:51 -0800

    Render context menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ import { CIRCLE as CIRCLE_CONFIG } from '../geometry_helpers/shapes_config'
 import { getArrowProps } from '../geometry_helpers/shapes_config'
 import { coordinatesToSvgPoints } from '../geometry_helpers/general'
 import { useArray } from '../hooks/useArray'
+import { PositionWrapper } from './common/Wrappers.jsx'
 import { Editor } from './Editor.jsx'
 
 const SVG_HEIGHT = 500
@@ -19,6 +20,15 @@ const SVG_WIDTH = 750
 
 const StyledSvg = styled.svg`
   background: lightgrey;
+`
+
+const ContextMenu = styled.div`
+  height: 100px;
+  width: 100px;
+  background: white;
+  position: absolute;
+  left: ${props => props.left || 0}px;
+  top: ${props => props.top || 0}px
 `
 
 const getVertexById = ({ vertices, id }) => vertices[id]
@@ -119,8 +129,18 @@ const handleMouseMove = ({ event, setDraggedVertxId, draggedVertexId, vertices, 
   }
 }
 
-const handleContextClick = event => {
+const handleContextClick = ({
+  event,
+  setIsContextMenuOpen,
+  setContextMenuLocation
+}) => {
   event.preventDefault()
+
+  setIsContextMenuOpen(true)
+  setContextMenuLocation({
+    x: event.clientX,
+    y: event.clientY
+  })
 }
 
 const Circle = ({
@@ -153,6 +173,9 @@ const App = props => {
   const [draggedVertexId, setDraggedVertxId] = useState(null)
   const [vertices, setVertices] = useState(DEFAULT_VERTICES)
   const [edges, setEdges] = useState(DEFAULT_EDGES)
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false)
+  const [contextMenuLocation, setContextMenuLocation] = useState({})
+
   const {
     state: arrows,
     push: createArrow,
@@ -171,33 +194,47 @@ const App = props => {
 
   return (
     <>
-      <StyledSvg
-        width={SVG_WIDTH}
-        height={SVG_HEIGHT}
-        onMouseMove={event => handleMouseMove({ ...commonProps, event })}
-        onMouseUp={() => setDraggedVertxId(null)}
-        onContextMenu={handleContextClick}
-      >
-        {
-          edges.map((edge, index) => {
-            return renderEdge({ edge, index, vertices, arrows })
-          })
-        }
-        {renderArrows({ arrows, edges, vertices })}
-        {
-          Object.keys(vertices).map((vertexId, index) => {
-            return (
-              <Circle
-                id={vertexId}
-                x={vertices[vertexId].x}
-                y={vertices[vertexId].y}
-                index={index}
-                setDraggedVertxId={setDraggedVertxId}
-              />
-            )
-          })
-        }
-      </StyledSvg>
+      <PositionWrapper>
+        <StyledSvg
+          width={SVG_WIDTH}
+          height={SVG_HEIGHT}
+          onMouseMove={event => handleMouseMove({ ...commonProps, event })}
+          onMouseUp={() => setDraggedVertxId(null)}
+          onContextMenu={event => handleContextClick({
+            event,
+            setIsContextMenuOpen,
+            setContextMenuLocation
+          })}
+        >
+          {
+            edges.map((edge, index) => {
+              return renderEdge({ edge, index, vertices, arrows })
+            })
+          }
+          {renderArrows({ arrows, edges, vertices })}
+          {
+            Object.keys(vertices).map((vertexId, index) => {
+              return (
+                <Circle
+                  id={vertexId}
+                  x={vertices[vertexId].x}
+                  y={vertices[vertexId].y}
+                  index={index}
+                  setDraggedVertxId={setDraggedVertxId}
+                />
+              )
+            })
+          }
+        </StyledSvg>
+        {isContextMenuOpen && (
+          <ContextMenu
+            left={contextMenuLocation.x}
+            top={contextMenuLocation.y}
+          >
+            blah
+          </ContextMenu>
+        )}
+      </PositionWrapper>
       <Editor
         vertices={vertices}
         setVertices={setVertices}
---
+++ src/components/common/Wrappers.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const PositionWrapper = styled.div`
+  position: ${props => props.position || 'relative'};
+`
