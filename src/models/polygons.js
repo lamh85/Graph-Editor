b4d33c03f99bf17cedbdd3bd2c0b4e0e7cdf1789
Hon Lam <hon@junglescout.com>
2021-02-06 17:16:51 -0800

    Remove unused variable.

--- src/models/polygons.js
+++ src/models/polygons.js
@@ -1,39 +1,3 @@
-export const DEFAULT_EDGES = [
-  {
-    id: 1,
-    end0: {
-      vertexId: 1,
-      arrowType: 'triangle'
-    },
-    end1: {
-      vertexId: 2,
-      arrowType: null
-    }
-  },
-  {
-    id: 2,
-    end0: {
-      vertexId: 2,
-      arrowType: 'triangle'
-    },
-    end1: {
-      vertexId: 3,
-      arrowType: null
-    }
-  },
-  {
-    id: 3,
-    end0: {
-      vertexId: 3,
-      arrowType: 'triangle'
-    },
-    end1: {
-      vertexId: 1,
-      arrowType: null
-    }
-  }
-]
-
 export const DEFAULT_VERTICES = [
   { id: 1, x: 40, y: 40, radius: 20 },
   { id: 2, x: 200, y: 40, radius: 20 },
