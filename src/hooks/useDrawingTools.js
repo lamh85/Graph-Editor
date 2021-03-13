de0f3edff3cb2ba394f66e97a623835de259e858
Hon Lam <hon@junglescout.com>
2021-03-12 21:50:47 -0800

    Create/update vertex functions in mouse tools.

--- src/hooks/useDrawingTools.js
+++ src/hooks/useDrawingTools.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 
+import { getDistance } from '../geometry_helpers/get_distance'
+import { getHypotenuseLength } from '../geometry_helpers/trigonometry'
+import { RADIUS_MINIMUM } from '../models/vertices'
+
 const TOOL_NAMES = {
   MOVE: 'MOVE',
   RESIZE: 'RESIZE',
@@ -12,13 +16,61 @@ const TOOL_BLOCKERS = {
   [TOOL_NAMES.RESIZE]: [TOOL_NAMES.DROP]
 }
 
-const createVertexByDrawing = crudPayload => {}
+export const createRectangleWithDrag = ({
+  rectangleProps,
+  createVertex
+}) => {
+  if (!rectangleProps) return null
 
-const buildResizedRadius = crudPayload => {
+  const { top, left, width, height } = rectangleProps
 
+  const centreX = left + width / 2
+  const centreY = top + height / 2
+
+  createVertex({
+    centreX,
+    centreY,
+    height,
+    width,
+    shape: 'rectangle'
+  })
 }
 
-export const useDrawingTools = () => {
+export const createCircleWithDrag = ({
+  circleProps,
+  createVertex
+}) => {
+  if (!circleProps) return null
+
+  if (circleProps.radius < RADIUS_MINIMUM) return
+
+  createVertex({
+    ...circleProps,
+    shape: 'circle'
+  })
+}
+
+const buildResizedRadius = ({ crudPayload, currentCoordinates }) => {
+  const raidusTriangle = getDistance({
+    origin: {
+      x: crudPayload.vertex.centreX,
+      y: crudPayload.vertex.centreY
+    },
+    destination: currentCoordinates
+  })
+
+  const newRadius = getHypotenuseLength({
+    adjacent: raidusTriangle.height,
+    opposite: raidusTriangle.width
+  })
+
+  if (!newRadius || newRadius < RADIUS_MINIMUM) return null
+
+  return newRadius
+}
+
+// TODO: ensure that clickCoordinates must be canvas coordinates, not the screen coordinates
+export const useDrawingTools = ({ updateVertex, createVertex }) => {
   const [selectedTool, setSelectedTool] = useState(null)
   const [clickCoordinates, setClickCoordinates] = useState({
     x: null,
@@ -52,12 +104,85 @@ export const useDrawingTools = () => {
     if (![TOOL_NAMES.MOVE, TOOL_NAMES.RESIZE].includes(selectedTool)) {
       return
     }
+
+    let propertySet = {}
+
+    if (selectedTool === TOOL_NAMES.RESIZE) {
+      propertySet = buildResizedRadius({ crudPayload, currentCoordinates })
+    } else if (selectedTool === TOOL_NAMES.MOVE) {
+      propertySet = {
+        centreX: currentCoordinates.x,
+        centreY: currentCoordinates.y
+      }
+    }
+
+    if (!propertySet) return
+
+    updateVertex({
+      id: crudPayload.vertex.id,
+      propertySet
+    })
   }
 
   useEffect(updateVertexWithMouseMove, [currentCoordinates])
 
+  const hasCoordinates =
+    clickCoordinates?.x &&
+    clickCoordinates?.y &&
+    currentCoordinates?.x &&
+    currentCoordinates?.y
+
+  const rectangleProps = () => {
+    if (!hasCoordinates) return
+
+    const left = Math.min(clickCoordinates.x, currentCoordinates.x)
+    const top = Math.min(clickCoordinates.y, currentCoordinates.y)
+    const height = Math.abs(currentCoordinates.y - clickCoordinates.y)
+    const width = Math.abs(currentCoordinates.x - clickCoordinates.x)
+
+    return { left, top, height, width }
+  }
+
+  const circleProps = () => {
+    if (!hasCoordinates) return
+
+    const { x: x1, y: y1 } = clickCoordinates || {}
+    const { x: x2, y: y2 } = currentCoordinates || {}
+
+    if (!x1 || !y1 || !x2 || !y2) return null
+
+    const centreX = (x1 + x2) / 2
+    const centreY = (y1 + y2) / 2
+
+    const radius = getHypotenuseLength({
+      adjacent: centreX - x1,
+      opposite: centreY - y1
+    })
+
+    if (!radius) return null
+
+    return {
+      centreX,
+      centreY,
+      radius,
+      cx: centreX,
+      cy: centreY,
+      r: radius
+    }
+  }
+
   const mouseUpListener = () => {
-    createVertexByDrawing(crudPayload)
+    if (selectedTool === TOOL_NAMES.DRAW) {
+      paintbrushShape === 'rectangle'
+        && createRectangleWithDrag({ rectangleProps, createVertex })
+
+      paintbrushShape === 'circle'
+        && createCircleWithDrag({ circleProps, createVertex })
+    }
+
+    if ([TOOL_NAMES.DRAW, TOOL_NAMES.MOVE, TOOL_NAMES.RESIZE].includes(selectedTool)) {
+      stopTool()
+    }
   }
 
   return {
