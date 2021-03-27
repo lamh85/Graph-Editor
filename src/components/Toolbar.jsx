c84d2f4fbe523d5717ca1318d4e0f2a31eb7c3d1
lamh85 <lamh85@gmail.com>
2021-03-27 16:22:47 -0700

    Style the toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,12 +1,19 @@
 import React from "react"
+import styled from 'styled-components'
+
 import { MenuItem } from './common/MenuItem.jsx'
+
+const StyleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export const Toolbar = ({
   extraOptions,
   drawingTools
 }) => {
   return (
-    <>
+    <StyleWrapper>
       <MenuItem onClick={drawingTools.stopTool}>
         Default mode
       </MenuItem>
@@ -30,6 +37,6 @@ export const Toolbar = ({
           </MenuItem>
         )
       })}
-    </>
+    </StyleWrapper>
   )
 }
