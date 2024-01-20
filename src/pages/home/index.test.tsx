eb3131893a52d1072faa28bbc05552ccde326950
Hon Lam <lamh85@gmail.com>
2024-01-19 21:13:07 -0800

    Test: mouse drag a circle. (#15)
    * Configure Jest to accept Typescript in test files. WIP: test dragging a shape.
    
    * Test: mouse drag a circle.

--- src/pages/home/index.test.tsx
+++ src/pages/home/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import Home from '.'
+import { render, fireEvent } from '@testing-library/react'
+import Home from './index.jsx'
 
 describe('Home.jsx', () => {
   beforeEach(() => {
@@ -16,6 +16,25 @@ describe('Home.jsx', () => {
 
       expect(circles.length).toEqual(2)
       expect(rectangles.length).toEqual(1)
+    })
+  })
+
+  describe('Core cursor features', () => {
+    it('Move a circle', async () => {
+      const circle = document.querySelector('[class*="CircleInner"]')
+
+      const xBefore = circle.getAttribute('cx')
+      const yBefore = circle.getAttribute('cy')
+
+      fireEvent.mouseDown(circle)
+      fireEvent.mouseMove(circle, { clientX: 10, clientY: 15 })
+      fireEvent.mouseUp(circle)
+
+      const xAfter = circle.getAttribute('cx')
+      const yAfter = circle.getAttribute('cy')
+
+      expect(parseInt(xAfter) - parseInt(xBefore)).toEqual(10)
+      expect(parseInt(yAfter) - parseInt(yBefore)).toEqual(15)
     })
   })
 })
